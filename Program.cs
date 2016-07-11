241557597bc6260f14b2cfddab183d23ae3b1ce6
joejjbh <joejjbh@hotmail.co.uk>
2016-07-11 13:13:48 +0100

    Update Program.cs
    added commenting

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace CardProject
 {
@@ -6,12 +6,15 @@ namespace CardProject
     {
         public static void Main(string[] args)
         {
+            // initialise new instance if Deck
             Deck myDeck = new Deck();
+            // run the shuffle() method in the Deck class
             myDeck.Shuffle();
             for (int i = 0; i < 52; i++)
             {
+                // how the cards are displayed
                 Console.Write("{0,-19}", myDeck.DealCard());
-                if ((i + 1) % 4 == 0)
+                if ((i + 1) % 4 == 0) // 4 cards on each line
                 {
                     Console.WriteLine();
                 }
