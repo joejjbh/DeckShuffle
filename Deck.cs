dfb48ed1cc8a34b71d2f4e1e95b348f71737a309
joejjbh <joejjbh@hotmail.co.uk>
2016-07-11 13:00:56 +0100

    Update Deck.cs
    added commenting

--- Deck.cs
+++ Deck.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace CardProject
 {
@@ -11,12 +11,18 @@ namespace CardProject
 
         public Deck()
         {
+            // state the possible face values of the cards into an array
             string[] values = { "Ace", "Two", "Three", "Four", "Five", "Six",
                                 "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+            // state the possible suits that the cards can have into an array
             string[] suits =  { "Hearts", "Clubs", "Diamonds", "Spades" };
+            // initialise a new instance of Card that has the number of cards as 52
             deck = new Card[NUMBER_OF_CARDS];
+            // set the current current card to 0
             currentCard = 0;
+            // create a new instance of Random()
             ranNum = new Random();
+            // populate the deck
             for (int count = 0; count < deck.Length; count++)
             {
                 deck[count] = new Card(values[count % 13], suits[count / 13]);
@@ -28,10 +34,10 @@ namespace CardProject
             currentCard = 0;
             for (int first = 0; first < deck.Length; first++)
             {
-                int second = ranNum.Next(NUMBER_OF_CARDS);
-                Card temp = deck[first];
-                deck[first] = deck[second];
-                deck[second] = temp;
+                int second = ranNum.Next(NUMBER_OF_CARDS); // get a random number from the 52 possible cards
+                Card temp = deck[first]; // store the first card in variable temp
+                deck[first] = deck[second]; //  store second into first 
+                deck[second] = temp; // store temp into second
             }
         }
 
